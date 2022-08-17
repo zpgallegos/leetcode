3e95a6f5975649c8135b0f85de5a027ac21f6225
Zachary Gallegos <zpgallegos@gmail.com>
2022-08-16 17:46:32 -0700

    2153

--- code/2142.sql
+++ code/2142.sql
@@ -1,16 +1,31 @@
 -- https://leetcode.com/problems/the-number-of-passengers-in-each-bus-i/
-
-with cte as (
-    select
+WITH cte AS (
+    SELECT
         bus_id,
-        arrival_time as bus_time,
-        coalesce(lag(arrival_time, 1) over(order by arrival_time), 0) as last_bus
-    from buses
+        arrival_time AS bus_time,
+        coalesce(
+            lag(arrival_time, 1) over(
+                ORDER BY
+                    arrival_time
+            ),
+            0
+        ) AS last_bus
+    FROM
+        buses
 )
-
-select * from (
-    select bus_id, count(b.passenger_id) as passengers_cnt
-    from cte a 
-        left join Passengers b on b.arrival_time > a.last_bus and b.arrival_time <= a.bus_time
-    group by bus_id
-) s order by bus_id;
+SELECT
+    *
+FROM
+    (
+        SELECT
+            bus_id,
+            count(b.passenger_id) AS passengers_cnt
+        FROM
+            cte a
+            LEFT JOIN Passengers b ON b.arrival_time > a.last_bus
+            AND b.arrival_time <= a.bus_time
+        GROUP BY
+            bus_id
+    ) s
+ORDER BY
+    bus_id;
--- code/2153.sql
+++ code/2153.sql
@@ -1,2 +1,66 @@
 -- https://leetcode.com/problems/the-number-of-passengers-in-each-bus-ii/
 
+WITH recursive bus AS (
+    SELECT
+        *,
+        row_number() over(
+            ORDER BY
+                arrival_time
+        ) AS bus_n,
+        lag(arrival_time, 1, 0) over(
+            ORDER BY
+                arrival_time
+        ) AS last_bus_arrival_time
+    FROM
+        buses
+),
+pass AS (
+    SELECT
+        bus.bus_id,
+        bus.bus_n,
+        bus.capacity,
+        count(pas.passenger_id) AS passengers
+    FROM
+        bus
+        LEFT JOIN Passengers pas ON pas.arrival_time <= bus.arrival_time
+        AND pas.arrival_time > bus.last_bus_arrival_time
+    GROUP BY
+        bus.bus_id,
+        bus.bus_n,
+        bus.capacity
+),
+cte AS (
+    SELECT
+        bus_id,
+        bus_n,
+        IF(passengers <= capacity, passengers, capacity) AS picked_up,
+        IF(passengers > capacity, passengers - capacity, 0) AS waiting
+    FROM
+        pass
+    WHERE
+        bus_n = 1
+    UNION
+    SELECT
+        pass.bus_id,
+        pass.bus_n,
+        IF(
+            passengers + waiting <= capacity,
+            passengers + waiting,
+            capacity
+        ),
+        IF(
+            passengers + waiting > capacity,
+            passengers + waiting - capacity,
+            0
+        )
+    FROM
+        cte
+        INNER JOIN pass ON cte.bus_n + 1 = pass.bus_n
+)
+SELECT
+    bus_id,
+    picked_up AS passengers_cnt
+FROM
+    cte
+ORDER BY
+    bus_id;
