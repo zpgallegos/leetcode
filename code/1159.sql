926e1992c2a35f36eac676ced71c79a410fca6d1
Zachary Gallegos <zpgallegos@gmail.com>
2022-08-31 10:22:49 -0700

    608, 1159

--- code/1159.sql
+++ code/1159.sql
@@ -1,49 +1,26 @@
 -- https://leetcode.com/problems/market-analysis-ii/
-WITH ranked AS (
-    SELECT
-        orders.seller_id,
-        rank() over(
-            PARTITION by orders.seller_id
-            ORDER BY
-                orders.order_date
-        ) AS order_no,
-        items.item_brand = users.favorite_brand AS is_favorite
-    FROM
-        orders
-        INNER JOIN users ON orders.seller_id = users.user_id
-        INNER JOIN items ON orders.item_id = items.item_id
-),
-cte AS (
-    SELECT
-        seller_id,
-        is_favorite
-    FROM
-        ranked
-    WHERE
-        order_no = 2
-)
-SELECT
-    seller_id,
-    IF(is_favorite, 'yes', 'no') AS 2nd_item_fav_brand
-FROM
-    (
-        SELECT
-            *
-        FROM
-            cte
-        UNION
-        (
-            SELECT
-                user_id AS seller_id,
-                0 AS is_favorite
-            FROM
-                users
-            WHERE
-                user_id NOT IN (
-                    SELECT
-                        seller_id
-                    FROM
-                        cte
-                )
-        )
+
+with seconds as (
+    select *
+    from (
+        select
+            *,
+            rank() over(partition by seller_id order by order_date) as rnk
+        from orders
     ) q
+    where rnk = 2
+)
+
+select
+    seconds.seller_id,
+    if(users.favorite_brand = items.item_brand, 'yes', 'no') as 2nd_item_fav_brand
+
+from seconds
+    inner join users on seconds.seller_id = users.user_id
+    inner join items on seconds.item_id = items.item_id
+
+union
+
+select user_id as seller_id, 'no' as 2nd_item_fav_brand
+from users
+where user_id not in(select seller_id from seconds)
--- code/608.sql
+++ code/608.sql
@@ -1,22 +1,14 @@
+-- https://leetcode.com/problems/tree-node/
 
+select distinct
+    a.id,
+    case
+    when a.p_id is null then 'Root'
+    when b.p_id is not null then 'Inner'
+    else 'Leaf'
+    end as type
 
+from tree a
+    left join tree b on a.id = b.p_id
 
-select * from (
-    select
-        id, 
-        'Root' as type
-    from Tree
-    where p_id is null
-
-    union
-
-    -- leaves
-
-    select
-        a.id,
-        case when b.p_id is not null then 'Inner' else 'Leaf' end as type
-    from Tree a left join Tree b on a.id = b.p_id
-    where a.p_id is not null
-) sub
-order by sub.id;
-
+order by a.id;
