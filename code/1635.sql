c3f9b47dcdb7cc59c2ec3bc3955438d6af1551bf
Zachary Gallegos <zpgallegos@gmail.com>
2022-09-07 10:22:10 -0700

    1635

--- code/1635.sql
+++ code/1635.sql
@@ -1,86 +1,54 @@
 -- https://leetcode.com/problems/hopper-company-queries-i/
-WITH recursive months AS (
-    SELECT
-        1 AS mnth
-    UNION
-    SELECT
-        mnth + 1
-    FROM
-        months
-    WHERE
-        mnth < 12
+
+with recursive all_months as (
+    select 1 as month
+    union
+    select month + 1 from all_months where month < 12
 ),
-drive AS (
-    SELECT
-        months.mnth,
-        coalesce(
-            max(q.n_drivers) over(
-                ORDER BY
-                    months.mnth
-            ),
-            0
-        ) AS active_drivers
-    FROM
-        months
-        LEFT JOIN (
-            SELECT
-                mnth,
-                max(n_drivers) AS n_drivers
-            FROM
-                (
-                    SELECT
-                        join_date,
-                        MONTH(join_date) AS mnth,
-                        count(1) over(
-                            ORDER BY
-                                join_date
-                        ) AS n_drivers
-                    FROM
-                        drivers
-                ) s
-            WHERE
-                year(join_date) = 2020
-            GROUP BY
-                mnth
-        ) q ON months.mnth = q.mnth
+d as (
+    select
+        all_months.month,
+        coalesce(max(q.active_drivers), 0) as active_drivers
+
+    from all_months
+        left join (
+            select
+                month,
+                max(active_drivers) as active_drivers
+            
+            from (
+                select
+                    *,
+                    month(join_date) as month,
+                    row_number() over(order by join_date) as active_drivers
+                from drivers
+            ) s
+            where year(join_date) = 2020
+            group by month
+        ) q on q.month <= all_months.month
+    
+    group by all_months.month
 ),
-accept AS (
-    SELECT
-        MONTH(rides.requested_at) AS mnth,
-        count(acc.ride_id) AS acc
-    FROM
-        rides
-        LEFT JOIN acceptedrides acc ON rides.ride_id = acc.ride_id
-    WHERE
-        year(rides.requested_at) = 2020
-    GROUP BY
-        MONTH(rides.requested_at)
+r as (
+    select
+        month(a.requested_at) as month,
+        count(1) as accepted_rides
+    from rides a
+        inner join acceptedrides b on a.ride_id = b.ride_id
+    where year(a.requested_at) = 2020
+    group by month(a.requested_at)
 ),
-rides AS (
-    SELECT
-        *
-    FROM
-        accept
-    UNION
-    SELECT
-        mnth,
-        0 AS acc
-    FROM
-        months
-    WHERE
-        mnth NOT IN(
-            SELECT
-                mnth
-            FROM
-                accept
-        )
+all_r as (
+    select * from r
+    
+    union
+    
+    select all_months.month, 0 as accepted_rides 
+    from all_months
+        left join r on all_months.month = r.month
+    where r.month is null
 )
-SELECT
-    drive.mnth AS MONTH,
-    drive.active_drivers,
-    rides.acc AS accepted_rides
-FROM
-    drive
-    INNER JOIN rides ON drive.mnth = rides.mnth
-ORDER BY
-    drive.mnth
+
+select d.month, d.active_drivers, all_r.accepted_rides
+from d inner join all_r on d.month = all_r.month
+order by d.month;
