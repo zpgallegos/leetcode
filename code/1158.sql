e087d5e7211c6e9ff8608175ce00ef342c8e5c27
Zachary Gallegos <zpgallegos@gmail.com>
2022-11-06 11:55:08 -0800

    1158

--- code/1158.sql
+++ code/1158.sql
@@ -1,17 +1,18 @@
+-- https://leetcode.com/problems/market-analysis-i/
 
+with cte as (
+    select
+        buyer_id,
+        count(1) as orders_in_2019
+    from orders
+    where year(order_date) = 2019
+    group by buyer_id
+)
 
 select
     a.user_id as buyer_id,
     a.join_date,
-    coalesce(sub.orders, 0) as orders_in_2019
+    coalesce(b.orders_in_2019, 0) as orders_in_2019
 
-from 
-    Users a left join (
-        select buyer_id, count(distinct order_id) as orders
-        from Orders
-        where year(order_date) = 2019
-        group by buyer_id
-    ) sub on a.user_id = sub.buyer_id
-
-
-
+from users a
+    left join cte b on a.user_id = b.buyer_id;
