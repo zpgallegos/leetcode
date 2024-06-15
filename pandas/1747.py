26595c326da80a20200fed00aa296df53c8bb050
Zachary Gallegos <zpgallegos@gmail.com>
2024-06-14 21:49:55 -0700

    1747

---
+++ pandas/1747.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/leetflex-banned-accounts/
+
+
+import pandas as pd
+
+
+def leetflex_banned_accnts(log_info: pd.DataFrame) -> pd.DataFrame:
+    return (
+        log_info.merge(log_info, on="account_id")
+        .query(
+            """
+            ip_address_x != ip_address_y 
+            and login_y >= login_x 
+            and login_y <= logout_x
+            """
+        )[["account_id"]]
+        .drop_duplicates()
+    )
--- sql/1747.sql
+++ sql/1747.sql
@@ -1,15 +1,10 @@
 -- https://leetcode.com/problems/leetflex-banned-accounts/
 
 
-with a as (
-    select
-        *,
-        lag(a.logout, 1) over(partition by a.account_id order by a.login) as last_logout,
-        lag(a.ip_address, 1) over(partition by a.account_id order by a.login) as last_ip
-    
-    from LogInfo a
-)
-
 select distinct a.account_id
-from a
-where a.login <= a.last_logout and a.ip_address != a.last_ip;
+from loginfo a join loginfo b
+where
+    a.account_id = b.account_id and
+    a.ip_address != b.ip_address and
+    b.login >= a.login and
+    b.login <= a.logout;
