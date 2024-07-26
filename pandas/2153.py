b9b2c00cb66393c96dc1baa2fc0fa05edace52dd
Zachary Gallegos <zpgallegos@gmail.com>
2024-07-25 18:55:47 -0700

    2153"

---
+++ pandas/2153.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/the-number-of-passengers-in-each-bus-ii/solutions/
+
+import pandas as pd
+
+
+def number_of_passengers(buses: pd.DataFrame, passengers: pd.DataFrame) -> pd.DataFrame:
+    passengers["waiting"] = True
+    passengers = passengers.sort_values("arrival_time").set_index("passenger_id")
+
+    buses = buses.set_index("bus_id")
+
+    out = {}
+    for bus in buses.itertuples():
+        out[bus.Index] = 0
+        capacity = bus.capacity
+
+        waiting = passengers[
+            (passengers.waiting) & (passengers.arrival_time <= bus.arrival_time)
+        ]
+        for p in waiting.itertuples():
+            out[bus.Index] += 1
+            passengers.loc[p.Index, "waiting"] = False
+            capacity -= 1
+            if not capacity:
+                break
+
+    return pd.DataFrame(out.items(), columns=["bus_id", "passengers_cnt"]).sort_values(
+        "bus_id"
+    )
--- sql/2153.sql
+++ sql/2153.sql
@@ -1,66 +1,47 @@
 -- https://leetcode.com/problems/the-number-of-passengers-in-each-bus-ii/
 
-WITH recursive bus AS (
-    SELECT
-        *,
-        row_number() over(
-            ORDER BY
-                arrival_time
-        ) AS bus_n,
-        lag(arrival_time, 1, 0) over(
-            ORDER BY
-                arrival_time
-        ) AS last_bus_arrival_time
-    FROM
-        buses
-),
-pass AS (
-    SELECT
-        bus.bus_id,
-        bus.bus_n,
-        bus.capacity,
-        count(pas.passenger_id) AS passengers
-    FROM
-        bus
-        LEFT JOIN Passengers pas ON pas.arrival_time <= bus.arrival_time
-        AND pas.arrival_time > bus.last_bus_arrival_time
-    GROUP BY
-        bus.bus_id,
-        bus.bus_n,
-        bus.capacity
-),
-cte AS (
-    SELECT
+
+with recursive bus as (
+    select
         bus_id,
-        bus_n,
-        IF(passengers <= capacity, passengers, capacity) AS picked_up,
-        IF(passengers > capacity, passengers - capacity, 0) AS waiting
-    FROM
-        pass
-    WHERE
-        bus_n = 1
-    UNION
-    SELECT
-        pass.bus_id,
-        pass.bus_n,
-        IF(
-            passengers + waiting <= capacity,
-            passengers + waiting,
-            capacity
-        ),
-        IF(
-            passengers + waiting > capacity,
-            passengers + waiting - capacity,
-            0
-        )
-    FROM
-        cte
-        INNER JOIN pass ON cte.bus_n + 1 = pass.bus_n
+        row_number() over(order by a.arrival_time) as bus_n,
+        a.arrival_time,
+        lag(a.arrival_time, 1, 0) over(order by a.arrival_time) as last_bus_arrival_time,
+        a.capacity
+    from buses a
+),
+new as (
+    select
+        a.bus_id,
+        a.bus_n,
+        a.capacity,
+        count(b.passenger_id) as pass_cnt
+    from bus a
+        left join passengers b on
+            b.arrival_time <= a.arrival_time and
+            b.arrival_time > a.last_bus_arrival_time
+    group by 1, 2, 3
+),
+cte as (
+    select
+        a.bus_id,
+        a.bus_n,
+        if(a.pass_cnt >= a.capacity, a.capacity, a.pass_cnt) as picked_up,
+        if(a.pass_cnt > a.capacity, a.pass_cnt - a.capacity, 0) as waiting
+    from new a
+    where a.bus_n = 1
+    
+    union all
+    
+    select
+        b.bus_id,
+        b.bus_n,
+        if(b.pass_cnt + a.waiting >= b.capacity, b.capacity, b.pass_cnt + a.waiting) as picked_up,
+        if(b.pass_cnt + a.waiting > b.capacity, b.pass_cnt + a.waiting - b.capacity, 0) as waiting
+    from cte a
+        inner join new b on a.bus_n + 1 = b.bus_n
 )
-SELECT
-    bus_id,
-    picked_up AS passengers_cnt
-FROM
-    cte
-ORDER BY
-    bus_id;
+
+select a.bus_id, a.picked_up as passengers_cnt
+from cte a
+order by a.bus_id;
