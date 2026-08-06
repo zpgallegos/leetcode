cf3d4d770117a0f637465c2c2a3cd6f0a8735800
Zachary Gallegos <zpgallegos@gmail.com>
2026-08-06 15:52:17 -0700

    2199

--- pandas/2199.py
+++ pandas/2199.py
@@ -2,22 +2,40 @@
 
 import pandas as pd
 
+data = [[1, "handball"], [1, "football"], [3, "WAR"], [2, "Vaccine"]]
+keywords = pd.DataFrame(data, columns=["topic_id", "word"]).astype(
+    {"topic_id": "Int64", "word": "object"}
+)
+data = [
+    [1, "We call it soccer They call it football hahaha"],
+    [2, "Americans prefer basketball while Europeans love handball and football"],
+    [3, "stop the war and play handball"],
+    [4, "warning I planted some flowers this morning and then got vaccinated"],
+]
+posts = pd.DataFrame(data, columns=["post_id", "content"]).astype(
+    {"post_id": "Int64", "content": "object"}
+)
+
+
+def get_post_topics(content: str, keywords: pd.DataFrame) -> str:
+    tokens = content.lower().split()
+
+    matches = set()
+    for row in keywords.itertuples():
+        if row.word in tokens:
+            matches.add(row.topic_id)
+
+    if not matches:
+        return "Ambiguous!"
+
+    return ",".join(map(str, sorted(matches)))
+
 
 def find_topic(keywords: pd.DataFrame, posts: pd.DataFrame) -> pd.DataFrame:
-    tokenize = lambda doc: doc.lower().split(" ")
-    posts["tokens"] = posts.content.apply(tokenize)
 
-    kw = keywords.groupby("topic_id").word.apply(lambda x: set(x.str.lower())).to_dict()
+    keywords = keywords.assign(word=lambda df: df.word.str.lower())
 
-    def get_topics(tokens: list[str]) -> str:
-        out = []
-        for post_id, keywords in kw.items():
-            for keyword in keywords:
-                if keyword in tokens:
-                    out.append(post_id)
-                    break
-        return ",".join(map(str, out)) if out else "Ambiguous!"
+    return posts.assign(
+        topic=lambda df: df.content.apply(lambda x: get_post_topics(x, keywords))
+    ).drop(columns="content")
 
-    posts["topic"] = posts.tokens.apply(get_topics)
-
-    return posts[["post_id", "topic"]]
