66fae0fea9e742d492b49e924a75594d17254fa9
Zachary Gallegos <zpgallegos@gmail.com>
2026-08-06 11:44:34 -0700

    2153

--- pandas/2153.py
+++ pandas/2153.py
@@ -1,26 +1,36 @@
 # https://leetcode.com/problems/the-number-of-passengers-in-each-bus-ii/description/
 
+import numpy as np
 import pandas as pd
+
+buses_data = [[1, 2, 1], [2, 4, 10], [3, 7, 2]]
+buses = pd.DataFrame(buses_data, columns=["bus_id", "arrival_time", "capacity"]).astype(
+    {"bus_id": "Int64", "arrival_time": "Int64", "capacity": "Int64"}
+)
+
+passengers_data = [[11, 1], [12, 1], [13, 5], [14, 6], [15, 7]]
+passengers = pd.DataFrame(
+    passengers_data, columns=["passenger_id", "arrival_time"]
+).astype({"passenger_id": "Int64", "arrival_time": "Int64"})
 
 
 def number_of_passengers(buses: pd.DataFrame, passengers: pd.DataFrame) -> pd.DataFrame:
-    passengers["waiting"] = True
-    passengers = passengers.sort_values("arrival_time")
 
     out = []
-    for bus in buses.sort_values("arrival_time").itertuples():
-        waiting = passengers[
-            (passengers.waiting) & (passengers.arrival_time <= bus.arrival_time)
-        ]
-        
-        taken = 0
-        for passenger in waiting.itertuples():
-            if taken == bus.capacity:
-                break
+    waiting = 0
+    last_arrival_time = 0
 
-            passengers.loc[passenger.Index, "waiting"] = False
-            taken += 1
-        
-        out.append({"bus_id": bus.bus_id, "passengers_cnt": taken})
-    
+    for row in buses.sort_values("arrival_time").itertuples():
+
+        waiting += (
+            (passengers.arrival_time > last_arrival_time)
+            & (passengers.arrival_time <= row.arrival_time)
+        ).sum()
+
+        picked_up = min(int(row.capacity), waiting)
+        waiting -= picked_up
+        last_arrival_time = row.arrival_time
+
+        out.append({"bus_id": row.bus_id, "passengers_cnt": picked_up})
+
     return pd.DataFrame(out).sort_values("bus_id")
--- sql/2153.sql
+++ sql/2153.sql
@@ -1,45 +1,132 @@
 -- https://leetcode.com/problems/the-number-of-passengers-in-each-bus-ii/description/
 
-with recursive bus as (
-    select 
-        a.*,
-        row_number() over win as bus_idx,
-        lag(a.arrival_time, 1, 0) over win as last_arrival
-    from buses a
-    window win as (order by a.arrival_time)
-),
-cte as (
+/*
+2153. The Number of Passengers in Each Bus II
+Solved
+Hard
+Topics
+SQL Schema
+Pandas Schema
+Table: Buses
+
++--------------+------+
+| Column Name  | Type |
++--------------+------+
+| bus_id       | int  |
+| arrival_time | int  |
+| capacity     | int  |
++--------------+------+
+bus_id contains unique values.
+Each row of this table contains information about the arrival time of a bus at the LeetCode station and its capacity (the number of empty seats it has).
+No two buses will arrive at the same time and all bus capacities will be positive integers.
+ 
+
+Table: Passengers
+
++--------------+------+
+| Column Name  | Type |
++--------------+------+
+| passenger_id | int  |
+| arrival_time | int  |
++--------------+------+
+passenger_id contains unique values.
+Each row of this table contains information about the arrival time of a passenger at the LeetCode station.
+ 
+
+Buses and passengers arrive at the LeetCode station. If a bus arrives at the station at a time tbus and a passenger arrived at a time tpassenger where tpassenger <= tbus and the passenger did not catch any bus, the passenger will use that bus. In addition, each bus has a capacity. If at the moment the bus arrives at the station there are more passengers waiting than its capacity capacity, only capacity passengers will use the bus.
+
+Write a solution to report the number of users that used each bus.
+
+Return the result table ordered by bus_id in ascending order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Buses table:
++--------+--------------+----------+
+| bus_id | arrival_time | capacity |
++--------+--------------+----------+
+| 1      | 2            | 1        |
+| 2      | 4            | 10       |
+| 3      | 7            | 2        |
++--------+--------------+----------+
+Passengers table:
++--------------+--------------+
+| passenger_id | arrival_time |
++--------------+--------------+
+| 11           | 1            |
+| 12           | 1            |
+| 13           | 5            |
+| 14           | 6            |
+| 15           | 7            |
++--------------+--------------+
+Output: 
++--------+----------------+
+| bus_id | passengers_cnt |
++--------+----------------+
+| 1      | 1              |
+| 2      | 1              |
+| 3      | 2              |
++--------+----------------+
+Explanation: 
+- Passenger 11 arrives at time 1.
+- Passenger 12 arrives at time 1.
+- Bus 1 arrives at time 2 and collects passenger 11 as it has one empty seat.
+
+- Bus 2 arrives at time 4 and collects passenger 12 as it has ten empty seats.
+
+- Passenger 12 arrives at time 5.
+- Passenger 13 arrives at time 6.
+- Passenger 14 arrives at time 7.
+- Bus 3 arrives at time 7 and collects passengers 12 and 13 as it has two empty seats.
+*/
+
+with recursive buses_ordered as (
     select
-        a.bus_idx,
+        *,
+        row_number() over win as bus_idx,
+        lag(arrival_time, 1, 0) over win as last_arrival_time
+    from buses
+    window win as (order by arrival_time)
+),
+
+base as (
+    select
         a.bus_id,
+        a.bus_idx,
         a.capacity,
-        count(b.passenger_id) as since_last    
-    from bus a left join passengers b on -- left for since_last=0 when no new passengers arrive for a bus
-        1=1
-        and b.arrival_time > a.last_arrival 
-        and b.arrival_time <= a.arrival_time
+        count(passenger_id) as since_last -- n passengers arrived since last bus
+    from buses_ordered a
+    left join passengers b on
+        b.arrival_time <= a.arrival_time -- passenger arrived before this bus
+        and b.arrival_time > a.last_arrival_time -- passenger arrived after last bus
     group by 1, 2, 3
 ),
-taken as (
-    select
-        a.bus_idx,
-        a.bus_id,
-        case when a.since_last >= a.capacity then a.capacity else a.since_last end as picked_up,
-        case when a.since_last > a.capacity then a.since_last - a.capacity else 0 end as waiting
-    from cte a
-    where a.bus_idx = 1
 
-    union
+counts as (
+    select
+        bus_id,
+        bus_idx,
+        least(since_last, capacity) as picked_up,
+        greatest(since_last - capacity, 0) as waiting
+    from base
+    where bus_idx = 1
+
+    union all
 
     select
-        a.bus_idx,
         a.bus_id,
-        case when a.since_last + b.waiting >= a.capacity then a.capacity else a.since_last + b.waiting end,
-        case when a.since_last + b.waiting > a.capacity then a.since_last + b.waiting - a.capacity else 0 end
-    from cte a
-        inner join taken b on a.bus_idx = b.bus_idx + 1
+        a.bus_idx,
+        least(a.since_last + b.waiting, a.capacity) as picked_up,
+        greatest(a.since_last + b.waiting - a.capacity, 0) as waiting
+    from base a
+    inner join counts b on a.bus_idx = b.bus_idx + 1
 )
 
 select bus_id, picked_up as passengers_cnt
-from taken
+from counts
 order by 1;
